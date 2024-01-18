160a364a25a3fe8b22f0f2ef527785beef3bac8d
Kishore Kumar S <livekishore2001@gmail.com>
2024-01-18 18:50:19 +0530

    Update 18-01-2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:expense_app/widgets/expenses.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 // var kColorScheme = ColorScheme.fromSeed(
 //     seedColor: const Color.fromARGB(255, 96, 56, 181),
@@ -16,67 +15,40 @@ void main() {
     DynamicColorBuilder(
       builder: (lightDynamic, darkDynamic) => MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Builder(
-          builder: (context) {
-            bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
-            Color? statusBarColor = isDarkMode
-                ? lightDynamic
-                    ?.onPrimaryContainer // Set the color for light mode
-                : darkDynamic
-                    ?.onPrimaryContainer; // Set the color for dark mode
-
-            Brightness statusBarBrightness = isDarkMode
-                ? Brightness.dark // Set brightness for light mode
-                : Brightness.light; // Set brightness for dark mode
-            return AnnotatedRegion<SystemUiOverlayStyle>(
-              value: SystemUiOverlayStyle(
-                  statusBarColor: statusBarColor,
-                  statusBarBrightness:
-                      statusBarBrightness // Set the color you want
-                  ),
-              child: MaterialApp(
-                theme: ThemeData(
-                  colorScheme: lightDynamic,
-                  useMaterial3: true,
-                ).copyWith(
-                  appBarTheme: const AppBarTheme().copyWith(
-                    backgroundColor: lightDynamic?.onPrimaryContainer,
-                    foregroundColor: lightDynamic?.primaryContainer,
-                  ),
-                  cardTheme: CardTheme(
-                    color: lightDynamic?.secondaryContainer,
-                    margin:
-                        const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-                  ),
-                  elevatedButtonTheme: ElevatedButtonThemeData(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: lightDynamic?.primaryContainer,
-                      foregroundColor: lightDynamic?.onPrimaryContainer,
-                    ),
-                  ),
-                ),
-                darkTheme: ThemeData(
-                  colorScheme: darkDynamic,
-                  useMaterial3: true,
-                ).copyWith(
-                  colorScheme: darkDynamic,
-                  cardTheme: CardTheme(
-                    color: darkDynamic?.secondaryContainer,
-                    margin:
-                        const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-                  ),
-                  elevatedButtonTheme: ElevatedButtonThemeData(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: darkDynamic?.primaryContainer,
-                      foregroundColor: darkDynamic?.onPrimaryContainer,
-                    ),
-                  ),
-                ),
-                home: const Expenses(),
-              ),
-            );
-          },
+        theme: ThemeData(
+          colorScheme: lightDynamic,
+          useMaterial3: true,
+        ).copyWith(
+            appBarTheme: const AppBarTheme().copyWith(
+              backgroundColor: lightDynamic?.onPrimaryContainer,
+              foregroundColor: lightDynamic?.primaryContainer,
+            ),
+            cardTheme: CardTheme(
+              color: lightDynamic?.secondaryContainer,
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+            ),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+                style: ElevatedButton.styleFrom(
+              backgroundColor: lightDynamic?.primaryContainer,
+              foregroundColor: lightDynamic?.onPrimaryContainer,
+            ))),
+        darkTheme: ThemeData(
+          colorScheme: darkDynamic,
+          useMaterial3: true,
+        ).copyWith(
+          colorScheme: darkDynamic,
+          cardTheme: CardTheme(
+            color: darkDynamic?.secondaryContainer,
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: darkDynamic?.primaryContainer,
+              foregroundColor: darkDynamic?.onPrimaryContainer,
+            ),
+          ),
         ),
+        home: const Expenses(),
       ),
     ),
 
